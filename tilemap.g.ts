3bf393b62d269ab3ee630b8a086d38475ec76890
ottivoli <76687015+ottivoli@users.noreply.github.com>
2020-12-28 12:42:41 +0100

    Rakete!

---
+++ tilemap.g.ts
@@ -0,0 +1,7 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
